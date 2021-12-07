9c20378442f62ff2fc40b4a68542d167baa39f12
Иксан <iksan168@abv.bg>
2021-12-07 11:32:12 +0200

    MidExamPreparation-Solved

---
+++ MidExamPreparation/BonusScoringSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BonusScoringSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            int numOfLectures = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+            double maxBonus = 0;
+            double total = 0;
+            int student = 0;
+            for (int i = 1; i <= numberOfStudents; i++)
+            {
+                int attendances = int.Parse(Console.ReadLine());
+                total = ((double)attendances / (double)numOfLectures) * (5 + additionalBonus);
+                if (total > maxBonus)
+                {
+                    maxBonus = total;
+                    student = attendances;
+                }
+            }
+            Console.WriteLine($"Max Bonus: {Math.Round(maxBonus)}.\nThe student has attended {student} lectures.");
+        }
+    }
+}
