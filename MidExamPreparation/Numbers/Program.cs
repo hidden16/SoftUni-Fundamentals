e9ad04935b8aee64e6b06118c5721879f74ffe00
Иксан <iksan168@abv.bg>
2021-12-07 11:25:41 +0200

    MidExamPreparation-Solved

---
+++ MidExamPreparation/Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> kekw = new List<int>();
+            double avgNum = 0;
+            if (numbers.Count == 1)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                avgNum += numbers[i];
+            }
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (avgNum / numbers.Count == numbers[0])
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            numbers.Sort();
+            numbers.Reverse();
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < numbers.Count)
+                {
+                    if (numbers[i] > avgNum / numbers.Count)
+                    {
+                        Console.Write(numbers[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
