96981f1971bf5e3f22d2c1d22d1721f857d5c326
Иксан <iksan168@abv.bg>
2021-12-07 11:23:25 +0200

    MidExamPreparation-Solved

---
+++ MidExamPreparation/SoftuniReception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SoftuniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            int hours = 0;
+            while (students > 0)
+            {
+                hours++;
+                students -= first;
+                students -= second;
+                students -= third;
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
