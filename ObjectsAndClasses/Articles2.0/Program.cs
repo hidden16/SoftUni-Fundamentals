024e5da5bd2e94b3d276786c3eb5eb16f08e50a6
Иксан <iksan168@abv.bg>
2021-12-09 01:00:04 +0200

    ObjectsAndClasses-Solved

---
+++ ObjectsAndClasses/Articles2.0/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Articles2._0
+{
+    class Articles
+    {
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public Articles(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return $"{Title} -{Content}:{Author}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Articles> articles = new List<Articles>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(",").ToArray();
+
+                var article = new Articles(input[0], input[1], input[2]);
+
+                articles.Add(article);
+            }
+            string orderByWhat = Console.ReadLine();
+
+            switch (orderByWhat)
+            {
+                case "title":
+                    articles = articles.OrderBy(article => article.Title).ToList();
+                    break;
+                case "content":
+                    articles = articles.OrderBy(article => article.Content).ToList();
+                    break;
+                case "author":
+                    articles = articles.OrderBy(article => article.Author).ToList();
+                    break;
+
+                default:
+                    break;
+            }
+            Console.WriteLine(string.Join("\n", articles));
+        }
+    }
+}
