de26b3a9165d822d683c1a3e438c72764c6d305c
Иксан <iksan168@abv.bg>
2021-12-10 21:20:01 +0200

    AssociativeArrays-Solved

---
+++ AssociativeArrays/CountCharsInAString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            char[] words = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != ' ')
+                {
+                    if (counts.ContainsKey(words[i]))
+                    {
+                        counts[words[i]]++;
+                    }
+                    else
+                    {
+                        counts.Add(words[i], 1);
+                    }
+                }
+            }
+            foreach (var word in counts)
+            {
+                Console.WriteLine($"{word.Key} -> {word.Value}");
+            }
+        }
+    }
+}
