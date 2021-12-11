6b5a870e14ae0f7f985564cb11d885b366e6e133
Иксан <iksan168@abv.bg>
2021-12-11 03:08:44 +0200

    TextProcessing-Solved

---
+++ TextProcessing/ValidUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextProcessingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ");
+            Regex regex1 = new Regex(@"^[a-zA-Z0-9_-]+$");
+            foreach (var word in words)
+            {
+                if (regex1.IsMatch(word) && word.Length >= 3 && word.Length <= 16)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
