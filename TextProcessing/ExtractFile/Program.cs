b7f2907818df5af4a73170f0627bfc67cd368727
Иксан <iksan168@abv.bg>
2021-12-11 03:09:52 +0200

    TextProcessing-Solved

---
+++ TextProcessing/ExtractFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = Console.ReadLine().Split("\\");
+            string splitted = file[file.Length - 1];
+            string[] extension = splitted.Split(".");
+            Console.WriteLine($"File name: {extension[0]}");
+            Console.WriteLine($"File extension: {extension[1]}");
+        }
+    }
+}
