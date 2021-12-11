2429c758b5349d6485ff7f8c4ef61a7663abdfe5
Иксан <iksan168@abv.bg>
2021-12-11 03:13:00 +0200

    TextProcessing-Solved

---
+++ TextProcessing/StringExplosion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int bomb = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (bomb > 0 && word[i] != '>')
+                {
+                    word = word.Remove(i, 1);
+                    bomb--;
+                    i--;
+                }
+                else if (word[i] == '>')
+                {
+                    bomb += int.Parse(word[i + 1].ToString());
+                }
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
