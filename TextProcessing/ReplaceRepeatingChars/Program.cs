24e53471fd18a77a7687558c5ca3d03c4757c32b
Иксан <iksan168@abv.bg>
2021-12-11 03:12:05 +0200

    TextProcessing-Solved

---
+++ TextProcessing/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] test = new char[word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                test = word.ToCharArray();
+                if (i + 2 <= word.Length)
+                {
+                    if (word[i] == word[i + 1])
+                    {
+                        word = word.Remove(i, 1);
+                        i = -1;
+                    }
+                }
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
