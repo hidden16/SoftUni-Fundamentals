dce96d93373d82d1164361930cec73906fc4065a
Иксан <iksan168@abv.bg>
2021-12-11 03:10:52 +0200

    TextProcessing-Solved

---
+++ TextProcessing/CaesarCipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CaesarSipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split("", StringSplitOptions.RemoveEmptyEntries);
+            char[] ch = new char[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                ch = words[i].ToCharArray();
+            }
+            foreach (char item in ch)
+            {
+                Console.Write((char)(item + 3));
+            }
+        }
+    }
+}
